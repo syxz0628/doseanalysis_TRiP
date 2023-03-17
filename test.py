db0908c4192ad82249e5cf04206aea21468a4b5d
syxz0628 <syxz0628@163.com>
2023-03-17 13:08:48 +0100

    111222

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import numpy as np
 import nrrd
 
 # 读取第一个NRRD文件
-data1, header1 = nrrd.read('/home/yurii/Sheng/patient_data/02/dose/TRiP_ana/3D-dose/1_Lung_P_10_10phys.nrrd')
+data1, header1 = nrrd.read('1_Lung_P_10_10phys.nrrd')
 # 初始化累加结果
 sum_data = data1.astype(np.float64)
-data, header = nrrd.read('/home/yurii/Sheng/patient_data/02/dose/TRiP_ana/3D-dose/1_Lung_P_10_11phys.nrrd')
+data, header = nrrd.read('1_Lung_P_10_11phys.nrrd')
 sum_data += data.astype(np.float64)
 #
 # # 循环读取剩余的NRRD文件
@@ -16,4 +16,4 @@ sum_data += data.astype(np.float64)
 #     sum_data += data.astype(np.float64)
 #
 # # 将累加结果保存为新的NRRD文件
-nrrd.write('/home/yurii/Sheng/patient_data/02/dose/TRiP_ana/3D-dose/sum.nrrd', sum_data, header1)
+nrrd.write('sum.nrrd', sum_data, header1)
