6c4f62ae6e9643c9ec7427200dc78397a837829a
syxz0628 <syxz0628@163.com>
2023-03-17 20:19:13 +0100

    111222

--- acc_dos_nrrd.py
+++ acc_dos_nrrd.py
@@ -5,6 +5,7 @@ import sys
 import related_funs
 import gamma
 import nrrd
+import subprocess
 
 
 class class_analysis_dos_nrrd:
@@ -27,7 +28,8 @@ class class_analysis_dos_nrrd:
         nrrd.write(self.path2doselist[-1]+'.nhdr', datafile1, headfile1,detached_header=True)
 
         print("finished writing of: ",self.path2doselist[-1][self.path2doselist[-1].rfind('/')+1:])
-
+        onehedfile=self.path2doselist[0][self.path2doselist[0].rfind('/')+1:].replace('.nrrd','hed')
+        self.fun_genhedchangename(self.path2doselist[-1],onehedfile)
     def fun_modifynrrdhed(self,path2dosenrrd):
         findwrongspace=False
         savelines=''
@@ -47,5 +49,14 @@ class class_analysis_dos_nrrd:
             with open(path2dosenrrd,'w+') as f:
                 f.writelines(savelines)
             print('nrrdfile: ',path2dosenrrd,' was modified the space directions')
+
+    def fun_genhedchangename(self,writetosenario,onehedfile):
+        with open(writetosenario+'.nhdr') as f:
+            lines=f.readlines()
+            lines=lines.replace('raw','dos')
+        with open(writetosenario+'.nhdr') as f:
+            f.writelines(lines)
+
+        subprocess.run(['cp', onehedfile, writetosenario+'.hed'])
 
 
